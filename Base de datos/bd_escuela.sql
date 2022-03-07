af5f908c5eb07b51771253ea53e5185edd9965aa
GeekAppleMan <Jaimehbo13@hotmail.com>
2022-03-07 11:13:49 -0700

    Update bd_escuela.sql

--- Base de datos/bd_escuela.sql
+++ Base de datos/bd_escuela.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-03-2022 a las 19:09:09
+-- Tiempo de generación: 07-03-2022 a las 19:13:26
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.1.1
 
