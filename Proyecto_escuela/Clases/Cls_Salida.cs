faa2116a47b8b4f1533f51d379def75b28c197a1
Rocky1399 <rocky99@live.com.mx>
2022-05-10 12:55:06 -0700

    update

--- Proyecto_escuela/Clases/Cls_Salida.cs
+++ Proyecto_escuela/Clases/Cls_Salida.cs
@@ -64,15 +64,17 @@ namespace Proyecto_escuela.Clases
             {
                 while (reader.Read())
                 {
-                    var request = WebRequest.Create(reader.GetString(1));
-                    using (var response = request.GetResponse())
-                    using (var stream = response.GetResponseStream())
-                    {
-                        System.Drawing.Image img = Bitmap.FromStream(stream);
-                        //img.RotateFlip(RotateFlipType.Rotate90FlipNone);
-                        grid.Rows.Add(img, reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4));
-                    }
-                  
+                    object imagen = reader.GetString(1);
+                    //var request = WebRequest.Create(reader.GetString(1));
+                    //using (var response = request.GetResponse())
+                    //using (var stream = response.GetResponseStream())
+                    //{
+                    //    System.Drawing.Image img = Bitmap.FromStream(stream);
+                    //    img.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                       
+                    //}
+                    grid.Rows.Add(imagen, reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4));
+
                 }
             }
             databaseConnection.Close();
@@ -81,7 +83,7 @@ namespace Proyecto_escuela.Clases
         public void CargarSalida(DataGridView grid)
         {
             grid.Rows.Clear();
-            string query = "SELECT tb_alumnos.id_alumno,foto_perfil matricula, nombres, apellidos FROM tb_alumnos Inner JOIN tb_salida on tb_alumnos.id_alumno=tb_salida.id_alumno WHERE id_estatus_salida='3' LIMIT 1";
+            string query = "SELECT tb_alumnos.id_alumno, foto_perfil, matricula, nombres, apellidos FROM tb_alumnos Inner JOIN tb_salida on tb_alumnos.id_alumno=tb_salida.id_alumno WHERE id_estatus_salida='3' LIMIT 1";
             MySqlConnection databaseConnection = new MySqlConnection(connectionString);
             MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
             commandDatabase.CommandTimeout = 60;
@@ -93,14 +95,16 @@ namespace Proyecto_escuela.Clases
             {
                 while (reader.Read())
                 {
-                    var request = WebRequest.Create(reader.GetString(1));
-                    using (var response = request.GetResponse())
-                    using (var stream = response.GetResponseStream())
-                    {
-                        System.Drawing.Image img = Bitmap.FromStream(stream);
-                        //img.RotateFlip(RotateFlipType.Rotate90FlipNone);
-                        grid.Rows.Add(img, reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4));
-                    }
+                    object imagen = reader.GetString(1);
+                    //var request = WebRequest.Create(reader.GetString(1));
+                    //using (var response = request.GetResponse())
+                    //using (var stream = response.GetResponseStream())
+                    //{
+                    //    System.Drawing.Image img = Bitmap.FromStream(stream);
+                    //    //img.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                        
+                    //}
+                    grid.Rows.Add(imagen, reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4));
                 }
             }
             databaseConnection.Close();
